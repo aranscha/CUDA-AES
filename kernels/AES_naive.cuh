203205a41a62e762d75e2304cc4cd558ee480055
RyanDeKoninck <RyanDaKing3@yahoo.com>
2022-11-29 23:21:21 -0500

    Initial implementation of naive AES kernel and FinalRound (does not yet include tests))

---
+++ kernels/AES_naive.cuh
@@ -0,0 +1,46 @@
+#define NR_ROUNDS 10
+
+__constant__ char rcon[256];
+__constant__ char sbox[256];
+__constant__ char mul2[256];
+__constant__ char mul3[256];
+
+/*
+   AES_naive is a naive implementation of the AES algorithm. It consists of:
+    1. KeyExpansion: create the ExpanedKey from the CipherKey
+    2. For each block (handled by a seperate thread) apply NR_ROUND - 1 Round
+       operations. Each Round consists of:
+        a. ByteSub operation
+        b. ShiftRows operation
+        c. ShiftColumns operation
+        d. AddRoundKey operation
+    3. For each block apply a single FinalRound operation. FinalRound consists of:
+        a. ByteSub operation
+        b. ShiftRows operation
+        c. AddRoundKey operations
+
+   InOut:
+    - State: char array of arbitrary length
+   Inputs:
+    - CipherKey: char array of length 16
+    - StateLength: length of the State array
+*/
+
+__global__ void AES_naive(char* State, char* CipherKey, const unsigned int StateLength)
+{
+    int index = (threadIdx.x + blockDim.x * blockIdx.x) * 16; // * 16 because every thread processes an entire block
+
+    // Only a single thread from the thread block must calculate the ExpanedKey
+    __shared__ char ExpandedKey[16 * (NR_ROUNDS + 1)];
+    if (index == 0)
+        KeyExpansion(CipherKey, ExpandedKey);
+
+    // Each thread handles 16 bytes (a single block) of the State
+    if (index + 16 <= StateLength)
+    {
+        AddRoundKey(State + index, ExpandedKey);
+        for (int i = 1; i < NR_ROUNDS; i++)
+            Round(State + index, ExpandedKey + 16 * i);
+        FinalRound(State + index, ExpandedKey + 16 * NR_ROUNDS)
+    }
+}
---
+++ kernels/FinalRound.cuh
@@ -0,0 +1,42 @@
+#ifdef TEST_FINALROUND
+
+#define NR_ROUNDS 10
+
+__constant__ char rcon[256];
+__constant__ char sbox[256];
+__constant__ char mul2[256];
+__constant__ char mul3[256];
+
+#endif
+
+/*
+   FinalRound operation:
+   Perform the following operations on the input block:
+    1. ByteSub
+    2. ShiftRows
+    3. AddRoundKey
+   This operation as a whole is the final round operation from the AES
+   algorithm. The RoundKey used is one block of the ExpandedKey.
+
+   Input:
+    - roundkey: char array of length 16
+
+   InOut:
+    - block: char array of length 16
+*/
+
+__device__ void FinalRound(char* block, char* roundkey)
+{
+    ByteSub(block);
+    ShiftRows(block);
+    AddRoundKey(block, roundkey);
+}
+
+#ifdef TEST_ROUND
+
+__global__ void FinalRoundTest(char* block, char* roundkey)
+{
+    FinalRound(block, roundkey);
+}
+
+#endif
